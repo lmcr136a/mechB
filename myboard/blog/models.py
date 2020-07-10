03099d53850ee66477c961090ddfad7e819b63f1
lmcr136a <59679444+lmcr136a@users.noreply.github.com>
2020-07-10 21:25:17 +0900

    z

--- myboard/blog/models.py
+++ myboard/blog/models.py
@@ -25,14 +25,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
+'''
     def get_absolute_url(self):
-        return reverse('blog:post_detail',args=(self.id,))
+       return reverse('blog:post_detail',args=(self.id,))
 
     def previous(self):
         return self.get_previous_by_mod_date()
 
     def get_next(self):
         return self.get_next_by_mod_date()
-
+'''
 
