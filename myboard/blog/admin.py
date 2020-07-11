64174a8acc2da62e87b14f2f26b6a2d96036ba80
lmcr136a <59679444+lmcr136a@users.noreply.github.com>
2020-07-11 17:51:05 +0900

    a

--- myboard/blog/admin.py
+++ myboard/blog/admin.py
@@ -3,7 +3,7 @@ from .models import Post
 # Register your models here.
 
 #admin.site.register(Post)
-admin.register(Post)
+@admin.register(Post)
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('id','title','mod_date')
