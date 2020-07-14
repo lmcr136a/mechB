8b01c757e4af1a7084af63ef14a582881e6b3a99
lmcr136a <59679444+lmcr136a@users.noreply.github.com>
2020-07-15 01:08:06 +0900

    a

--- myboard/blog/urls.py
+++ myboard/blog/urls.py
@@ -4,14 +4,14 @@ from . import views
 app_name='blog'
 
 urlpatterns =[
-    path('', views.PostLV, name='post_list'),
-    path('post/', views.PostLV, name='post_list'),
+    path('', views.PostLV, name='PostLV'),
+    path('post/', views.PostLV, name='PostLV'),
     path('post/<int:pk>/',views.PostDV,name='PostDV'),
-    path('archive/',views.PostAV,name='post_archive'),
-    path('archive/<int:year>/',views.PostYAV,name='post_year_archive'),
-    path('archive/<int:year>/<int:month>/',views.PostMAV,name='post_month_archive'),
-    path('archive/<int:year>/<int:month>/<int:day>/',views.PostDAV,name='post_day_archive'),
-    path('archive/today/',views.PostTAV,name='post_today'),
+    path('archive/',views.PostAV,name='PostAV'),
+    path('archive/<int:year>/',views.PostYAV,name='PostYAV'),
+    path('archive/<int:year>/<int:month>/',views.PostMAV,name='PostMAV'),
+    path('archive/<int:year>/<int:month>/<int:day>/',views.PostDAV,name='PostDAV'),
+    path('archive/today/',views.PostTAV,name='PostTAV'),
     path('post/new/',views.PostN, name='PostN'),
     path('post/<int:pk>/edit/',views.PostE, name='PostE')
 
